83271b06d7b7ab2aa16da83e61d59483f5537d1c
Yelich3201143 <3201143@sps-student.sk.ca>
2018-09-28 13:10:53 -0600

    terq

---
+++ State Scene/sketch.js
@@ -0,0 +1,31 @@
+// Goblin Friendship Simulator
+// Dylan Yelich
+// September 27
+//
+// Make Frend With Gobbo
+
+let door;
+let houseWindow;
+let goblinDoor1;
+let goblinDoor2;
+let house;
+let state;
+
+
+function preload() {
+  door = loadImage("assets/Door.png");
+  houseWindow = loadImage("assets/Window.png");
+  goblinDoor1 = loadImage("assets/Goblin 1D.png");
+  goblinDoor2 = loadImage("assets/Goblin 2D.png");
+  house = loadImage("assets/House Background.png");
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+
+}
